1eddfd278ddb91bef303bac2b0ff5235e7b67c27
gladiator06m <61495716+gladiator06m@users.noreply.github.com>
2022-02-06 17:28:40 +0100

    init test1 git

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,17 +1,17 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticDevCoffee = "ynov-v1";
 const assets = [
   "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/ynov.png",
-  "/images/ynov-2d-illustration.png",
-  "/images/ynov-creation-design.png",
-  "/images/ynov-informatique.png",
-  "/images/ynov-architecture-interieur.png",
-  "/images/ynov-marketing-communication.png",
-  "/images/ynov-audiovisuel.png",
-  "/images/ynov-web-management.png",
+  "../index.html",
+  "../css/style.css",
+  "../js/app.js",
+  "../images/ynov.png",
+  "../images/ynov-2d-illustration.png",
+  "../images/ynov-creation-design.png",
+  "../images/ynov-informatique.png",
+  "../images/ynov-architecture-interieur.png",
+  "../images/ynov-marketing-communication.png",
+  "../images/ynov-audiovisuel.png",
+  "../images/ynov-web-management.png",
 ];
 
 self.addEventListener("install", installEvent => {
